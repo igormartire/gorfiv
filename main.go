ba812541d722a8f5d857b88615c01170c0a6eac0
Igor Martire <igormartire@berkeley.edu>
2016-12-10 17:50:22 -0200

    implements filter, sort and pagination on invoice index

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"math"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 	"unicode/utf8"
 
@@ -14,15 +16,19 @@ import (
 
 type Invoice struct {
 	Id             int            `json:"id"`
-	CreatedAt      time.Time      `json:"created_at"`
-	ReferenceMonth int            `json:"reference_month"`
-	ReferenceYear  int            `json:"reference_year"`
+	CreatedAt      time.Time      `json:"createdAt"`
+	ReferenceMonth int            `json:"referenceMonth"`
+	ReferenceYear  int            `json:"referenceYear"`
 	Document       string         `json:"document"`
 	Description    string         `json:"description"`
 	Amount         float64        `json:"amount"`
-	IsActive       bool           `json:"is_active"`
-	DeactiveAt     mysql.NullTime `json:"deactive_at"`
+	IsActive       bool           `json:"isActive"`
+	DeactiveAt     mysql.NullTime `json:"deactiveAt"`
 }
+
+const (
+	RootPath = "localhost:3000"
+)
 
 func main() {
 	db, err := sql.Open("mysql",
@@ -35,6 +41,10 @@ func main() {
 	checkErr(err)
 
 	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/invoices")
+	})
 
 	router.GET("/invoices/:id", func(c *gin.Context) {
 		var invoice Invoice
@@ -58,11 +68,132 @@ func main() {
 
 	router.GET("/invoices", func(c *gin.Context) {
 		var (
-			invoice  Invoice
-			invoices []Invoice
+			invoice           Invoice
+			invoices          []Invoice
+			orderByStrs       []string
+			queryStr          string
+			filterClauseStr   string
+			numResultsPerPage int = 5
+			pageNumber        int = 1
+			linksHeader       []string
+			numActiveInvoices int
 		)
 
-		rows, err := db.Query("SELECT * FROM Invoice WHERE IsActive=1")
+		dbErr := db.QueryRow("SELECT COUNT(*) FROM Invoice WHERE IsActive=1").Scan(&numActiveInvoices)
+		if dbErr != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		perPageStr, isSet := c.GetQuery("perPage")
+		if isSet {
+			perPageVal, err := strconv.Atoi(perPageStr)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": "Malformed perPage parameter. It must be an integer.",
+				})
+				return
+			}
+			numResultsPerPage = perPageVal
+		}
+
+		lastPageNumber := int(math.Ceil(float64(numActiveInvoices) / float64(numResultsPerPage)))
+
+		pageNumStr, isSet := c.GetQuery("page")
+		if isSet {
+			pageNumVal, err := strconv.Atoi(pageNumStr)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": "Malformed page parameter. It must be an integer.",
+				})
+				return
+			}
+			if pageNumVal < 1 || pageNumVal > lastPageNumber {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": "Invalid page number passed as parameter.",
+				})
+				return
+			}
+			pageNumber = pageNumVal
+		}
+
+		linkPrefix := "<" + RootPath + "/invoices?"
+		values := c.Request.URL.Query()
+		if pageNumber < lastPageNumber {
+			//next
+			values.Set("page", strconv.Itoa(pageNumber+1))
+			fmt.Println(values.Encode())
+			linksHeader = append(linksHeader, linkPrefix+values.Encode()+">; rel=\"next\"")
+			//last
+			values.Set("page", strconv.Itoa(lastPageNumber))
+			fmt.Println(values.Encode())
+			linksHeader = append(linksHeader, linkPrefix+values.Encode()+">; rel=\"last\"")
+		}
+		if pageNumber > 1 {
+			//first
+			values.Set("page", "1")
+			linksHeader = append(linksHeader, linkPrefix+values.Encode()+">; rel=\"first\"")
+			//prev
+			values.Set("page", strconv.Itoa(pageNumber-1))
+			fmt.Println(values.Encode())
+			linksHeader = append(linksHeader, linkPrefix+values.Encode()+">; rel=\"prev\"")
+		}
+
+		documentFilter, filteringDocument := c.GetQuery("document")
+		if filteringDocument {
+			filterClauseStr += " AND Document=\"" + documentFilter + "\""
+		}
+		monthFilter, filteringMonth := c.GetQuery("referenceMonth")
+		if filteringMonth {
+			filterClauseStr += " AND ReferenceMonth=\"" + monthFilter + "\""
+		}
+		yearFilter, filteringYear := c.GetQuery("referenceYear")
+		if filteringYear {
+			filterClauseStr += " AND ReferenceYear=\"" + yearFilter + "\""
+		}
+
+		sortQuery, sorting := c.GetQuery("sort")
+		if sorting {
+			if sortQuery == "" {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": "malformed sort query",
+				})
+				return
+			}
+			fields := strings.Split(sortQuery, ",")
+			for _, field := range fields {
+				sort_order := "ASC"
+				if len(field) == 0 {
+					c.JSON(http.StatusBadRequest, gin.H{
+						"error": "malformed sort query",
+					})
+					return
+				}
+				if field[0] == '-' {
+					sort_order = "DESC"
+					field = field[1:]
+				}
+				if field == "document" || field == "referenceMonth" || field == "referenceYear" {
+					orderByStrs = append(orderByStrs, fmt.Sprintf("%v %v", field, sort_order))
+				} else {
+					c.JSON(http.StatusBadRequest, gin.H{
+						"error": "malformed sort query: can only order by document, referenceMonth and referenceYear",
+					})
+					return
+				}
+			}
+		}
+
+		queryStr = "SELECT * FROM Invoice WHERE IsActive=1" + filterClauseStr
+		if len(orderByStrs) > 0 {
+			queryStr += " ORDER BY " + strings.Join(orderByStrs, ", ")
+		}
+		queryStr += fmt.Sprint(" LIMIT ", (pageNumber-1)*numResultsPerPage, ", ", numResultsPerPage)
+
+		fmt.Println(queryStr)
+
+		rows, err := db.Query(queryStr)
+
 		if err != nil {
 			c.AbortWithError(http.StatusInternalServerError, err)
 			return
@@ -86,8 +217,9 @@ func main() {
 			return
 		}
 
-		c.Header("X-Total-Count", strconv.Itoa(len(invoices)))
-		c.JSON(http.StatusOK, gin.H{"collection": invoices})
+		c.Header("X-Total-Count", strconv.Itoa(numActiveInvoices))
+		c.Header("Link", strings.Join(linksHeader, ", "))
+		c.JSON(http.StatusOK, gin.H{"items": invoices})
 	})
 
 	router.POST("/invoices", func(c *gin.Context) {
@@ -143,7 +275,7 @@ func main() {
 			return
 		}
 
-		c.Header("Location", fmt.Sprint("localhost:3000/invoices", id))
+		c.Header("Location", fmt.Sprint(RootPath, "/invoices/", id))
 		c.Status(http.StatusCreated)
 	})
 
